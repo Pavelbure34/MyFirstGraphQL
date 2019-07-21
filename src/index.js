1a4310c8242579043d78b8497aa307829fee11bd
Dongbin Suh <dongbinsuh@gmail.com>
2019-07-21 13:58:01 +0900

    custom type practice done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {GraphQLServer} from 'graphql-yoga';
+//excellent module for graphQL development both for produciton and testing.
 
 /*
     With nodemon installed, you can actively update the server without turning it off.
     type definitions: application schema(data type, operations).
     without !: it is optional.
     with !: it must be certain type.(required)
+
+    How to pass data from Client to Server
+    
 */
 const typeDefs = `
     type Query{
@@ -14,6 +18,20 @@ const typeDefs = `
         year:Int!
         review:Float
         bankStatus:Float!
+        Me:Stat!
+        Other:[Enemy!]!
+    }
+
+    type Stat{
+        id:ID!
+        DEX:Int!
+        DEF:Int!
+        ATT:Int!
+    }
+
+    type Enemy{
+        id:ID!
+        HP:Int!
     }
 `;
 
@@ -25,29 +43,54 @@ const typeDefs = `
             3.Int: integer value. number
             4.Float: decimal values
             5.ID: represent identifiers(similar to String)
+    Custom Type data: our own custom data.
+        1. every custom data should start with capital letter.
+        2.In Resolver it should return the object which is  in sync
+            with typeDefs
+
 */
 
 //Resolvers:set of functions
 const resolvers = {
     Query:{
         //this have to match with typeDefs
-        org(){
+        org(){//String type
             return 'Defunct Co.';
         },
-        id(){
+        id(){//ID type
             return 'AAB004';
         },
-        active(){
+        active(){//Boolean type
             return false; 
         },
-        year(){
+        year(){//Int type
             return 2017;
         },
-        review(){
+        review(){//Float Type
             return;
         },
-        bankStatus(){
+        bankStatus(){//Float Type
             return 3.5;
+        },
+        Me(){//Custom Type
+            return {//usually data query from db is done here.
+                id:'myStat',
+                DEX:35,
+                DEF:30,
+                ATT:40
+            };
+        },
+        Other(){//using arrays and custom type.
+            return [
+                {
+                    id:'ABB01',
+                    HP:10
+                },
+                {
+                    id:'ABB02',
+                    HP:15
+                }
+            ]
         }
     }
 }
